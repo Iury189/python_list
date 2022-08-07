ddc39c2c92d59b7162637da4725e069aa6cba79b
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-08-07 09:06:00 -0300

    Update python_list.py

--- python_list.py
+++ python_list.py
@@ -9,29 +9,28 @@ while True:
             lista.append(numero)
         # Caso não seja o código se encerra
         else:
-            print ("Não é permitido números negativos, finalizando código...\n")
+            print ("Não é permitido números negativos, finalizando código...")
             break
         # Pergunta se quer continuar a inserir números
         print ("Deseja continuar inserindo números? (Não = 0 | Sim = 1)")
         pergunta = int(input("Digite sua escolha: "))
         # Caso a resposta seja NÃO o código se encerra
         if pergunta == 0:
-            print ("Finalizando código... \n")
+            print ("Finalizando código...")
             break
         # Caso a resposta seja SIM começa novamente o processo de inserção de números
         elif pergunta == 1:
             print ("Que bom que continua a inserir números dentro da lista.")
             print (f"Quantidade de números na lista: {len(lista)}")
-            print (f"{lista}\n")
+            print (f"{lista}")
             pass
         # Caso digite uma opção inválida o código se encerra
         else:
-            print (f"Opção {pergunta} desconhecida, finalizando código... \n")
+            print (f"Opção {pergunta} desconhecida, finalizando código...")
             break
-        # Caso o usuário digite uma opção inválida
+    # Caso o usuário digite uma opção inválida
     except Exception:
-        print ("Apenas números são permitidos, finalizando código... \n")
-        break
+        print ("Apenas números inteiros são permitidos, refaça a operação.")
 # Mensagem
 print (f"A quantidade de números dentro da lista é {len(lista)}.")
 print (lista)
